fb8c9d94d676c2126c777609c0918a150a369972
Letargikus-oposszum <b1vidtam@vasvari.org>
2026-02-24 12:25:05 +0100

    everything is done

--- src/pages/AllPizza.tsx
+++ src/pages/AllPizza.tsx
@@ -1,9 +1,36 @@
+import { useEffect, useState } from "react";
+import type { Pizza } from "../types/Pizza";
+import apiClient, { baseURL } from "../api/apiClient";
+import { toast } from "react-toastify";
+import { Button, Card, Carousel, Row } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+
 const AllPizza = () => {
-  return (
-    <>
-      <h1>404! Page not found</h1>
-    </>
-  );
+  const [pizzak, setPizzak] = useState<Array<Pizza>>([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    apiClient
+      .get("/pizzak")
+      .then((res) => setPizzak(res.data))
+      .catch(() => toast.error("Sikertelen betöltés!"));
+  }, []);
+
+  const createItem = (pizza: Pizza) => {
+    return (
+      <Card style={{ width: "18rem" }}>
+        <Card.Img src={`${baseURL}/kepek/${pizza.imageUrl}`} />
+        <Card.Title>{pizza.nev}</Card.Title>
+        <Card.Body>
+          {pizza.leiras}
+          <br />
+          {pizza.ar} FT
+        </Card.Body>
+      </Card>
+    );
+  };
+
+  return <><Row>{pizzak.map((pizza) => createItem(pizza))}</Row> <Button onClick={()=>{navigate("/new-pizza")}}>Új hozzáadása</Button></>;
 };
 
 export default AllPizza;
--- src/pages/NewPizza.tsx
+++ src/pages/NewPizza.tsx
@@ -1,7 +1,53 @@
+import { useEffect, useState } from "react";
+import type { Pizza } from "../types/Pizza";
+import apiClient from "../api/apiClient";
+import { toast } from "react-toastify";
+import { Button } from "react-bootstrap";
+
 const NewPizza = () => {
+  const [pizza, setPizza] = useState<Pizza>({
+    nev: "",
+    leiras: "",
+    ar: 0,
+    imageUrl: "",
+  });
+
+  const submit = () => {
+      apiClient
+        .post("/pizzak", pizza)
+        .then(() => {
+          toast.success("Sikeres hozzáadás!");
+        })
+        .catch(() => toast.error("Sikertelen hozzáadás!"));
+  };
+
   return (
     <>
-      <h1>404! Page not found</h1>
+      Név:{" "}
+      <input
+        type="text"
+        onChange={(e) => setPizza({ ...pizza, nev: e.target.value })}
+      />
+      Leiras:{" "}
+      <input
+        type="text"
+        onChange={(e) => setPizza({ ...pizza, leiras: e.target.value })}
+      />
+      Ár:{" "}
+      <input
+        type="text"
+        onChange={(e) => setPizza({ ...pizza, ar: Number(e.target.value) })}
+      />
+      Kép:{" "}
+      <input
+        type="text"
+        onChange={(e) => setPizza({ ...pizza, imageUrl: e.target.value })}
+      />
+      <Button
+        onClick={() => {
+          submit();
+        }}
+      >Hozzáadás</Button>
     </>
   );
 };
